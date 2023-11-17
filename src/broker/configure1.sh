b4e88a4aa4752f55979dc234f26a8c2782df9717
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2023-11-17 09:33:41 +0200

    update scripts

---
+++ src/broker/configure1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if a parameter has been provided
+if [ -z "$1" ]
+then
+    echo "Usage: ./configure.sh <ssh_user>"
+    exit 1
+fi
+
+# Define the ssh_user and create target_node
+ssh_user=$1
+
+iotlab-auth -u $ssh_user
+iotlab-experiment submit -n riot_mqtt -d 60 -l 1,archi=a8:at86rf231+site=grenoble
+iotlab-experiment wait
+iotlab-ssh --verbose wait-for-boot
+
+target_node=$(iotlab-experiment --jmespath="items[*].network_address | sort(@)" get --nodes)
+
+scp ./config.conf $ssh_user@$target_node:~/
+ssh root@$target_node
+
+# Print the global IPv6 address of the node
+ip -6 -o addr show eth0 | grep 'global'
+
+# Start the MQTT broker
+broker_mqtts config.conf
+
+EOF
---
+++ src/broker/copy_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if a parameter has been provided
+if [ -z "$1" ]
+then
+    echo "Usage: ./configure.sh <ssh_user>"
+    exit 1
+fi
+
+# Define the ssh_user and create target_node
+ssh_user=$1
+
+scp ./configure1.sh $ssh_user@grenoble.iot-lab.info:~/
+scp ./config.conf $ssh_user@grenoble.iot-lab.info:~/
