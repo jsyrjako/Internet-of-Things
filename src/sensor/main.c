773861f68d440d2690778e0db96502da8dad5ccc
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2023-11-22 11:14:19 +0200

    commit work in progress

--- src/sensor/main.c
+++ src/sensor/main.c
@@ -8,22 +8,49 @@
 
 #include "isl29020.h"
 
-// MQTT connection parameters
+// Get node's ID
+char* node_id = get_node_id();
+char* CLIENTID = node_id;
+
+// Initialize MQTTClient
 MQTTClient client;
-static const char *URL = "<node's address>:1886";
+static const char *URL = "<mqtt node's address>:1886";
+MQTTClient_create(&client, URL, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
 
-rc = MQTTClient_create(&client, URL, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-
+// Connect to MQTTClient
 MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-conn_opts.keepAliveInterval = 10;
-conn_opts.cleansession = 1;
-conn_opts.username = USERNAME;
-conn_opts.password = PASSWORD;
-rc = MQTTClient_connect(client, &conn_opts);
+
+// conn_opts.keepAliveInterval = 10;
+// conn_opts.cleansession = 1;
+// conn_opts.username = USERNAME;
+// conn_opts.password = PASSWORD;
+MQTTClient_connect(client, &conn_opts);
+printf("MQTT client connected\n");
+
+
+// Set up variables
+char topic[256];
+sprintf(topic, "sensor/data/%s", node_id);
+static const int QOS = 1;
+// MQTTClient_deliveryToken token;
 
 // Sensors
 static lpsxxx_t lpsxxx;
 static isl29020_t isl29020;
+
+int get_node_id()
+{
+    char hostname[1024];
+    hostname[1023] = '\0';
+    gethostname(hostname, 1023);
+    printf("Hostname: %s\n", hostname);
+    struct hostent* h;
+    h = gethostbyname(hostname);
+    printf("Hostent: %s\n", h->h_name);
+    char* node_id = h->h_name;
+    return node_id;
+}
+
 
 int init_sensors(void)
 {
@@ -85,15 +112,37 @@ int read_light()
     return light;
 }
 
+char* create_mqtt_message(uint16_t temp, uint16_t pres, uint16_t light)
+{
+    char* message = malloc(100);
+    sprintf(message, "{\"temperature\": %i.%u, \"pressure\": %uhPa, \"light\": %d}", (temp), (temp), pres, light);
+    printf("Message: %s\n", message);
+    return message;
+}
+
+void publish_message(char* message)
+{
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    pubmsg.payload = message;
+    pubmsg.payloadlen = strlen(message);
+    pubmsg.qos = QOS;
+    MQTTClient_publishMessage(client, topic, &pubmsg, &token);
+    printf("Message published\n");
+}
+
 int main(void)
 {
     init_sensors();
+    uint16_t temp, pres, light;
+    char* message;
 
     while (1)
     {
-        read_temperature();
-        read_pressure();
-        read_light();
+        temp = read_temperature();
+        pres = read_pressure();
+        light = read_light();
+        message = create_mqtt_message(temp, pres, light);
+        publish_message(message);
         ztimer_sleep(ZTIMER_MSEC, 5000);
     }
 
