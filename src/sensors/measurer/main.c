8566ffb672b26e182315a08c4cef0c4cd084b01d
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2023-11-24 08:40:18 +0200

    br + sensor nodes

---
+++ src/sensors/measurer/main.c
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2015 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Example application for demonstrating the RIOT network stack
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ztimer.h"
+#include "thread.h"
+#include "shell.h"
+#include "msg.h"
+
+#include "lpsxxx.h"
+#include "lpsxxx_params.h"
+#include "isl29020.h"
+#include "isl29020_params.h"
+
+// UDP
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+extern int udp_cmd(int argc, char **argv);
+
+static const shell_command_t shell_commands[] = {
+    { "udp", "send data over UDP and listen on UDP ports", udp_cmd },
+    { NULL, NULL, NULL }
+};
+
+// Sensing
+static lpsxxx_params_t lps_params = {
+    .i2c = lpsxxx_params[0].i2c,
+    .addr = lpsxxx_params[0].addr,
+    .rate = LPSXXX_RATE_1HZ,
+};
+
+static isl29020_params_t isl_params = {
+    .i2c = isl29020_params[0].i2c,
+    .addr = isl29020_params[0].addr,
+    .mode = ISL29020_MODE_AMBIENT,
+    .range = ISL29020_RANGE_1K,
+};
+
+static lpsxxx_t lpsxxx;
+static isl29020_t isl29020;
+
+static char stack_size[THREAD_STACKSIZE_DEFAULT];
+
+int init_sensors(void)
+{
+    // Initialize LPS331AP sensor
+    if (lpsxxx_init(&lpsxxx, &lps_params) != LPSXXX_OK)
+    {
+        perror("Failed to initialize sensor LPS331AP");
+        return 1;
+    }
+
+    // Initialize ISL29020 sensor
+    if (isl29020_init(&isl29020, &isl_params) != 0)
+    {
+        perror("Failed to initialize sensor LPS331AP");
+        return 2;
+    }
+    return 0;
+}
+
+int read_temperature(void)
+{
+    int16_t temp;
+    if (lpsxxx_read_temp(&lpsxxx, &temp) != LPSXXX_OK)
+    {
+        perror("Failed to read temperature");
+    }
+    return temp;
+}
+
+int read_pressure(void)
+{
+    uint16_t pres;
+    if (lpsxxx_read_pres(&lpsxxx, &pres) != LPSXXX_OK)
+    {
+        perror("Failed to read pressure");
+    }
+    return pres;
+}
+
+int read_light(void)
+{
+    int light = isl29020_read(&isl29020);
+    if (light == -1)
+    {
+        perror("Failed to read light");
+    }
+    return light;
+}
+
+static void *sensor_thread(void *arg)
+{
+    (void)arg;
+    int16_t temp;
+    uint16_t pres;
+    int light;
+
+    // char *message;
+    while (1)
+    {
+        temp = read_temperature();
+        pres = read_pressure();
+        light = read_light();
+        printf("Temperature: %i.%u°C\n", (temp / 100), (temp % 100));
+        printf("Pressure: %uhPa\n", pres);
+        printf("Light: %d\n", light);
+        // message = create_mqtt_message(temp, pres, light);
+        // publish_message(message);
+        ztimer_sleep(ZTIMER_MSEC, 5000);
+    }
+    return 0;
+}
+
+int main(void)
+{
+    /* we need a message queue for the thread running the shell in order to
+     * receive potentially fast incoming networking packets */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+    puts("RIOT network stack example application");
+
+    if (init_sensors() == 0)
+    {
+        thread_create(stack_size, sizeof(stack_size), THREAD_PRIORITY_MAIN - 1, 0, sensor_thread, NULL, "sensor_thread");
+    }
+
+    /* start shell */
+    puts("All up, running the shell now");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    /* should be never reached */
+    return 0;
+}
---
+++ src/sensors/start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+BOARD=iotlab-m3
+BR_ID=180
+SENSORS=181-185
+SITE=lille
+PAN_ID=0xa48f
+CHANNEL=24
+
+if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
+  # RIOT doesnt support the arm gcc version installed by default on the SSH frontend, e.g. 4.9.
+  # Therefore, we use arm gcc version 9 found in /opt/
+  source /opt/riot.source
+else
+  echo "[ERROR] Not in IoT-LAB frontend"
+fi
+
+iotlab-experiment submit -n "riot-ipv6" -d 120 -l archi=m3:at86rf231+site=$SITE
+
+iotlab-experiment wait --timeout 30 --cancel-on-timeout
+
+# Make the sensor nodes
+make -C ./measurer/ BOARD=$BOARD DEFAULT_CHANNEL=$CHANNEL DEFAULT_PAN_ID=$PAN_ID
+
+make -C ./br/gnrc_border_router ETHOS_BAUDRATE=500000 BOARD=$BOARD DEFAULT_CHANNEL=$CHANNEL DEFAULT_PAN_ID=$PAN_ID IOTLAB_NODE=m3-$BR_ID.$SITE.iot-lab.info flash
+
+if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
+  # Make sure that the monitoring profile exists
+  iotlab-profile del -n monitor_sensors_m3
+  iotlab-profile addm3 -n monitor_sensors_m3 -voltage -current -power -period 8244 -avg 16
+
+  # Flash the border router
+  cp br/gnrc_border_router/bin/${ARCH}/gnrc_border_router.elf ~/shared/
+  br=$(iotlab-experiment submit -n iot2023oulu_br -d 60 -l grenoble,m3,$BR_ID,~/shared/gnrc_border_router.elf,monitor_sensors_m3)
+  iotlab-experiment wait --timeout 30 --cancel-on-timeout
+
+  # Flash the sensor nodes
+  cp measurer/bin/${ARCH}/iot2023sensor.elf ~/shared/
+  measurers=$(iotlab-experiment submit -n iot2023oulu -d 60 -l grenoble,m3,$SENSORS,~/shared/iot2023sensor.elf,monitor_sensors_m3)
+  iotlab-experiment wait --timeout 30 --cancel-on-timeout
+
+  # Start ethos on the border router
+  sudo ethos_uhcpd.py m3-180 tap69 2001:660:5307:3169::/64
+
+fi
