52dcf2673d2aac050c697a3297148a3da03e18dc
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2023-11-29 09:10:27 +0200

    mqtt client säätö

--- src/sensors/measurer/main.c
+++ src/sensors/measurer/main.c
@@ -35,7 +35,7 @@
 #include "net/sock/udp.h"
 #include "net/ipv6/addr.h"
 
-#include "MQTTClient.h"
+#include "include/MQTTClient.h"
 
 // MQTT resources
 #define ADDRESS     "ssl://eu2.cloud.thethings.industries:1883"
