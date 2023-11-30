7c9849e688914119d03eca7d1d671787b689ea9e
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2023-11-30 08:34:59 +0200

    update ipv6 struct

--- src/sensors/measurer/coap_client.c
+++ src/sensors/measurer/coap_client.c
@@ -63,8 +63,8 @@ void send_to_coap_server(int16_t avg_temp, uint16_t avg_pres, int avg_light)
     }
 
     // Send the request
-    sock_udp_ep_t remote = { .family = AF_INET6, .port = COAP_SERVER_PORT } };
-    memcpy(remote.addr.ipv6, server_addr.u8, sizeof(remote.addr.ipv6));
+    sock_udp_ep_t remote = { .family = AF_INET6, .port = COAP_SERVER_PORT };
+    memcpy(remote.ipv6, server_addr.u8, sizeof(remote.ipv6));
 
     // Retry sending the request if it fails
     int retries = 0;
